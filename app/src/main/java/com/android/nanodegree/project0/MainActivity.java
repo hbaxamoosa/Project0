573017abc2e4276ba6a7d714e560bf8a69d4d8e1
hbaxamoosa <hbaxamoosa@gmail.com>
2015-08-31 11:51:28 -0500

    Incorporated feedback from Udacity reviewer.

--- app/src/main/java/com/android/nanodegree/project0/MainActivity.java
+++ app/src/main/java/com/android/nanodegree/project0/MainActivity.java
@@ -2,10 +2,7 @@ package com.android.nanodegree.project0;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -16,57 +13,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button01 = (Button) findViewById(R.id.button1);
-        Button button02 = (Button) findViewById(R.id.button2);
-        Button button03 = (Button) findViewById(R.id.button3);
-        Button button04 = (Button) findViewById(R.id.button4);
-        Button button05 = (Button) findViewById(R.id.button5);
-        Button button06 = (Button) findViewById(R.id.button6);
-
-        button01.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch Spotify Streamer", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button02.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch Scores App", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button03.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch Library App", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button04.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch Build It Bigger", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button05.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch XYZ Reader", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button06.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my Own App", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
+    public void spotifyStreamer(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Spotify Streamer", Toast.LENGTH_SHORT).show();
+    }
+
+    public void scoresApp(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Scores App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void libraryApp(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Library App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buildItBigger(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Build It Bigger", Toast.LENGTH_SHORT).show();
+    }
+
+    public void xyzReader(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch XYZ Reader", Toast.LENGTH_SHORT).show();
+    }
+
+    public void capstone(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch my Capstone App", Toast.LENGTH_SHORT).show();
+    }
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -87,5 +59,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
